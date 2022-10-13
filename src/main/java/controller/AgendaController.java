e7e579c5cf04edb726b1ff704ab63685d6bb3cc4
FredericoGSilva <frederico.dev@outlook.com>
2022-10-13 16:27:44 -0300

    AgendaController - atualizarTabela

---
+++ src/main/java/controller/AgendaController.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import controller.helper.AgendaHelper;
+import java.util.ArrayList;
+import model.Agendamento;
+import model.dao.AgendaDAO;
+import view.Agenda;
+
+/**
+ *
+ * @author Frederico
+ */
+public class AgendaController {
+    private final Agenda view;
+    private final AgendaHelper helper;
+    
+    public AgendaController(Agenda view) {
+        this.view = view;
+        this.helper = new AgendaHelper(view);
+    }
+    
+    public void atualizarTabela() {
+        AgendaDAO agendaDAO = new AgendaDAO();
+        ArrayList<Agendamento> listaAdendamento = agendaDAO.recuperDados();
+        helper.preencherTabela(listaAdendamento);
+    }
+
+    
+}
