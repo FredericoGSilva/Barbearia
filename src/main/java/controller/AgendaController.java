3b8d69cdcf351aba897c61fb6520d267aa84f6c2
FredericoGSilva <frederico.dev@outlook.com>
2022-10-13 16:05:50 -0300

    função recuperarDados

--- src/main/java/controller/AgendaController.java
+++ src/main/java/controller/AgendaController.java
@@ -4,28 +4,10 @@
  */
 package controller;
 
-import controller.helper.AgendaHelper;
-import java.util.ArrayList;
-import model.Agendamento;
-import model.dao.AgendaDAO;
-import view.Agenda;
-
 /**
  *
  * @author Frederico
  */
 public class AgendaController {
-    private final Agenda view;
-    private final AgendaHelper helper;
-
-    public AgendaController(Agenda view) {
-        this.view = view;
-        this.helper = new AgendaHelper(view);
-    }
     
-    public void atualizaTabela() {
-        AgendaDAO agendaDao = new AgendaDAO();
-        ArrayList<Agendamento> listaAgendamento = agendaDao.selecionaDados();
-        helper.dadosDoBanco(listaAgendamento);
-    }
 }
--- src/main/java/model/dao/AgendaDAO.java
+++ src/main/java/model/dao/AgendaDAO.java
@@ -4,10 +4,46 @@
  */
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import model.Agendamento;
+import model.Cliente;
+import model.Servico;
+
 /**
  *
  * @author Frederico
  */
 public class AgendaDAO {
-
+    Connection conector = new Banco().conector();
+    ArrayList<Agendamento> listaAgendamento = new ArrayList<>();
+    
+    public ArrayList<Agendamento> recuperDados() {
+        try {
+            String sql = "select * from tb_Agendamento";
+            PreparedStatement statement = conector.prepareStatement(sql);
+            statement.execute();
+            ResultSet resultSet = statement.getResultSet();
+            while (resultSet.next()) {
+                Cliente cliente = new Cliente();
+                Servico servico = new Servico();
+                int id = resultSet.getInt("id");
+                cliente.setNome(resultSet.getString("cliente"));
+                servico.setDescricao(resultSet.getString("servico"));
+                float valor = resultSet.getFloat("valor");
+                String data = resultSet.getString("dataAgendamento");
+                String hora = resultSet.getString("hora");
+                Agendamento agendamento = new Agendamento(id, cliente, servico, valor, data, hora);
+                listaAgendamento.add(agendamento);
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro na classe AgendaDAO: " + ex);
+        }
+        return listaAgendamento;
+    }
+    
 }
