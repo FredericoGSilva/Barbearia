a7d6d82d1907ae8d657c2f7074bddb2da2e3a06c
FredericoGSilva <frederico.dev@outlook.com>
2022-10-20 01:25:15 -0300

    CancelaAgendamentoController

---
+++ src/main/java/controller/CancelaAgendamentoController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import view.CancelaAgendamento;
+
+/**
+ *
+ * @author Frederico
+ */
+public class CancelaAgendamentoController {
+    
+    private final CancelaAgendamento view;
+
+    public CancelaAgendamentoController(CancelaAgendamento view) {
+        this.view = view;
+    }
+    
+    
+}
